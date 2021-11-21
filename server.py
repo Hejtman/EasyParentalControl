4ae801b8a1172a6b84659401b43976ca06a91d18
Michal Hejtmánek <michal.hejtmanek@avast.com>
2021-11-22 00:57:57 +0100

    +config modification via web

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ import bottle
 from logger import TerminalLogger
 from server_communication import ConfigSyncHandler
 
-DEBUG = True
+DEBUG = True  # FIXME: CLI or environ?
 NAME = 'Easy Parental Control'
 
 
@@ -31,7 +31,7 @@ def main():
         short_config = ''
         for i, c in enumerate(ConfigSyncHandler.configs.configs):
             short_config += f'''
-                <form action="/modify_time_left" method="post"><font size="5">{c.user if c.user else c.client_ip}:\t{c.time_left_min}</font>
+                <form action="/modify_time_left" method="post"><font size="10">{c.user if c.user else c.client_ip}:\t{c.time_left_min}</font>
                     <input name="{i}+10" value="+10" type="submit" style="font-size:50px"/>
                     <input name="{i}-10" value="-10" type="submit" style="font-size:50px"/>
                     <input name="{i}BAN" value="BAN" type="submit" style="font-size:50px"/>
@@ -47,15 +47,30 @@ def main():
                 c.time_left_today -= 600
             elif bottle.request.forms.get(f'{i}BAN'):
                 c.time_left_today = 0
-
         bottle.redirect("/")
 
     @bottle.route('/debug')
     def debug():  # TODO: set users name?
         ful_configs = ''
-        for c in ConfigSyncHandler.configs.configs:
-            ful_configs += '<br>'.join(str(c).split(', ')) + '<br><br><br>'
-        return f'<meta http-equiv="refresh" content="60"><H1>{NAME}</H1>{ful_configs}'
+        for i, c in enumerate(ConfigSyncHandler.configs.configs):
+            for line in str(c).replace('ClientConfiguration(', '').split(', '):
+                if '=' in line:
+                    key, value = line.split('=')
+                    ful_configs += f'<br>{key}=<input type="text" name="{i}{key}" value="{value}"/>'
+                else:
+                    ful_configs += f', {line}'
+        return f'<meta http-equiv="refresh" content="60"><H1>{NAME}</H1><form action="/modify_config" method="post">{ful_configs}<input type="submit"/></form>'
+
+    @bottle.post('/modify_config')
+    def modify_config():
+        for i, c in enumerate(ConfigSyncHandler.configs.configs):
+            for line in str(c).replace('ClientConfiguration(', '').split(', '):
+                if '=' in line:
+                    key, value = line.split('=')
+                    value = bottle.request.forms.get(f'{i}{key}').replace('\'', '')
+                    if isinstance(ConfigSyncHandler.configs.configs[i].__dict__[key], str) and ConfigSyncHandler.configs.configs[i].__dict__[key] != value:
+                        ConfigSyncHandler.configs.configs[i].__dict__[key] = value
+        bottle.redirect("/debug")
 
     s = ConfigSyncServer()
     t = threading.Thread(target=s.serve_forever)
