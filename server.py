60f75bac8fe2791a4abd36f1e805edec81c1e56c
Michal Hejtmánek <michal.hejtmanek@avast.com>
2021-12-19 16:28:10 +0100

    +bottle daemonisation

--- server.py
+++ server.py
@@ -3,40 +3,46 @@
 import socketserver
 import threading
 import bottle
+import daemon
 
 from logger import TerminalLogger
 from server_communication import ConfigSyncHandler
 from server_web_home import home
 from server_web_config import config
 
-DEBUG = True  # FIXME: CLI or environ?
-
 
 class ConfigSyncServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
-    allow_reuse_address = True
-
     def __init__(self, ip: str = ConfigSyncHandler.server_ip, port: int = ConfigSyncHandler.server_port, web_port: int = 8080, handler_class=ConfigSyncHandler):
         self.web_port = web_port
         self.logger = TerminalLogger(file_path=__file__.replace('py', 'log'))
         self.logger.debug(f'server init: {ip}:{port}')
-        try:
-            socketserver.TCPServer.__init__(self, (ip, port-1), handler_class)  # non-daemonised (first instance)
-        except OSError:
-            socketserver.TCPServer.__init__(self, (ip, port), handler_class)  # daemonised (second instance)
+        self.logger.info(f'handling {port}')
+        socketserver.TCPServer.__init__(self, (ip, port), handler_class)
+
+    def start(self):
+        self.handle_conf_requests()
+        self.start_web_loop()
+
+    def handle_conf_requests(self):
+        self.logger.debug('Starting handling config sync requests.')
+        t = threading.Thread(target=self.serve_forever)
+        t.daemon = True
+        t.start()
+        self.logger.debug('Handling config sync requests.')
 
     def start_web_loop(self):
+        self.logger.debug(f'Starting handling web config manipulation:{ConfigSyncHandler.server_ip}:{self.web_port}')
         b = bottle.Bottle()
         b.merge(home)
         b.merge(config)
-        b.run(host=ConfigSyncHandler.server_ip, port=self.web_port, reloader=DEBUG)
+        b.run(host=ConfigSyncHandler.server_ip, port=self.web_port)
+        self.logger.debug('Stopped handling web config manipulation.')
 
 
 def main():
-    s = ConfigSyncServer()
-    t = threading.Thread(target=s.serve_forever)
-    t.daemon = True
-    t.start()
-    s.start_web_loop()
+    with daemon.DaemonContext():
+        s = ConfigSyncServer()
+        s.start()
 
 
 if __name__ == "__main__":
